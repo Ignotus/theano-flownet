c3dd7457ebc975b17f1590d8d7f72bbb2bf423b2
Minh Ngo <nlminhtl@gmail.com>
2017-03-02 21:08:56 +0100

    Bug fixes

--- correlation_layer_test.py
+++ correlation_layer_test.py
@@ -9,17 +9,30 @@ data1 = np.random.rand(1, 2, 32, 32).astype(np.float32)
 
 data2 = np.random.rand(1, 2, 32, 32).astype(np.float32)
 
-op = CorrelationOp()
+op = CorrelationOp(data1.shape)
 
 t_data1 = T.ftensor4()
 t_data2 = T.ftensor4()
 
 t_outs = op(t_data1, t_data2)
 
-t_c = t_outs[0].sum()
+t_c = t_outs[2].sum()
 
-t_g_data = T.grad(t_c, t_data)[0]
+t_g_data = T.grad(t_c, t_data1)
 
-f = theano.function([t_data1, t_data2], t_outs + [t_g_data])
+# f = theano.function([t_data1, t_data2], t_outs)
 
-print(f(data1, data2))
+# rbot0, rbot1, out = f(data1, data2)
+
+# print('rbot0.shape', rbot0.shape)
+# print('rbot1.shape', rbot1.shape)
+# print('out.shape', out.shape)
+
+f = theano.function([t_data1, t_data2], [t_g_data], on_unused_input='warn')
+
+res = f(data1, data2)
+
+# print(res[0])
+# print(len(res))
+print(res[0].shape)
+print(res[0])
